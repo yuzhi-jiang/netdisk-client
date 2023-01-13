a1c5ba94d27fba6062a98240ba70b9b7820b5ee7
weimingfeng <luckywords@foxmail.com>
2023-01-13 10:01:28 +0800

    feat: add dynamic copyright

--- src/components/footer/index.vue
+++ src/components/footer/index.vue
@@ -1,6 +1,11 @@
 <template>
-  <a-layout-footer class="footer">© Netdisk</a-layout-footer>
+  <a-layout-footer class="footer">© {{ settings.copyright }}</a-layout-footer>
 </template>
+
+<script lang="ts" setup>
+  import * as settings from '@/config/settings.json';
+  // extension: provide an api to get dynamic copyright.
+</script>
 
 <style lang="less" scoped>
   .footer {
