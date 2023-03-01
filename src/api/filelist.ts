789906ca79660c8adaad766867cccfc49154fba6
weimingfeng <luckywords@foxmail.com>
2023-03-01 17:48:24 +0800

    feat: add filelist api

---
+++ src/api/filelist.ts
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import type { Pagination } from '@/types/global';
+
+export interface FolderRecord {
+  id: string;
+  parent_id: string;
+  name: string;
+  created_at: Date;
+  updated_at: Date;
+  type: 'file' | 'folder'; // convenience
+}
+
+export type FileRecord = {
+  file_extension: string;
+  category: string;
+  mime_type: string;
+  url: string;
+  thumbnail: string;
+  size: number;
+  meta: Record<string, any>;
+} & FolderRecord;
+
+export type NodeRecord = Partial<FileRecord>;
+
+export type FileParams = {
+  path: string;
+  filters: string;
+  sort: string;
+} & Pagination;
+
+export function getFileList(params?: Partial<FileParams>) {
+  return axios.get<NodeRecord[]>('/api/filelist', {
+    params,
+  });
+}
