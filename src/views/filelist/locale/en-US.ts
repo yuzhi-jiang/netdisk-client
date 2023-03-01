e1de98151831c0ce830d9f0326c7e5997353ce0c
weimingfeng <luckywords@foxmail.com>
2023-03-01 22:00:46 +0800

    feat: add locale language

---
+++ src/views/filelist/locale/en-US.ts
@@ -0,0 +1,7 @@
+export default {
+  'filelist.name': 'filename',
+  'filelist.tags': 'tags',
+  'filelist.updated_at': 'updated',
+  'filelist.type': 'type',
+  'filelist.size': 'size',
+};
---
+++ src/views/filelist/locale/zh-CN.ts
@@ -0,0 +1,7 @@
+export default {
+  'filelist.name': '名称',
+  'filelist.tags': '文件标签',
+  'filelist.updated_at': '修改日期',
+  'filelist.type': '类型',
+  'filelist.size': '大小',
+};
