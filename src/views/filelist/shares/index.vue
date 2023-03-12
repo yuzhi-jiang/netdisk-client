f2878e95dfa146cd9cde3d7aa4adaa0492756d61
weimingfeng <luckywords@foxmail.com>
2023-03-12 23:52:00 +0800

    feat: add shares page

--- src/views/filelist/shares/index.vue
+++ src/views/filelist/shares/index.vue
@@ -1,5 +1,183 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { ref } from 'vue';
+  import { useRoute } from 'vue-router';
+  import { IconMore } from '@arco-design/web-vue/es/icon';
+  import {
+    NodeRecord,
+    ReqParams,
+    ReqQueries,
+    getFileList,
+  } from '@/api/filelist';
+  import List from '@/components/list/index.vue';
+  import { IAction } from '@/components/list/types';
+  import { formatList, formatSize, paramsAdapter } from './utils';
+  import useList from './use-list';
+  import ModalForm from './components/modal-form.vue';
+  import ButtonAction from './components/button-action.vue';
+
+  const $route = useRoute();
+  const { columns, toolbar, actions } = useList();
+  const listRef = ref<InstanceType<typeof List>>();
+  const modalRef = ref<InstanceType<typeof ModalForm>>();
+  const states = {
+    reqParams: {} as ReqParams, // request params
+    reqQueries: {} as ReqQueries, // request queries
+  };
+
+  const onAction = async ({
+    action,
+    record,
+    selectedKeys,
+  }: {
+    action: IAction;
+    record?: NodeRecord;
+    selectedKeys?: number[];
+  }) => {
+    const { key } = action;
+    switch (key) {
+      case 'show.info':
+        console.log(record);
+        modalRef.value?.init(record);
+        break;
+      case 'bulk-delete':
+        listRef.value?.reload();
+        break;
+      default:
+    }
+  };
+
+  const onSuccess = () => {
+    listRef.value?.reload();
+  };
+
+  const request = async (params = {}) => {
+    params = paramsAdapter(params, states);
+    console.log(params);
+    const { data } = await getFileList(params);
+    if (data?.list) {
+      return {
+        // must be format
+        data: formatList(data.list),
+        total: data.total,
+      };
+    }
+    return {
+      data: [],
+      total: 0,
+    };
+  };
+
+  const init = () => {
+    /**
+     * 指定路由进入渲染，携带参数
+     * current route is filelist/all/:type/:parent_id
+     */
+    const { params, query } = $route;
+    // appoint a folder, can't find a way to define the route type, like type, parentId
+    if (Object.keys(params).length !== 0) {
+      const { type, parentId } = params as ReqParams;
+      states.reqParams = { type, parentId };
+      // get_path() // 获取fullpath
+    }
+    if (Object.keys(query).length !== 0) {
+      const { search } = query as ReqQueries;
+      states.reqQueries = { search };
+    }
+  };
+  init();
+</script>
 
 <template>
-  <div>Share</div>
+  <div>
+    <a-space direction="vertical" class="container">
+      <!-- <a-breadcrumb :max-count="maxCount" :routes="routes" /> -->
+      <List
+        ref="listRef"
+        :toolbar="toolbar"
+        :columns="columns"
+        :actions="[]"
+        :request="request"
+        @action="onAction"
+      >
+        <!-- acions.key -->
+        <template #batch-delete="{ action, onAction }: any">
+          <ButtonAction
+            :action="action"
+            :on-action="onAction"
+            icon="icon-shanchu"
+            icon-size="17"
+            name="list.actions.batch-delete"
+          />
+        </template>
+
+        <template #all-clear="{ action, onAction }: any">
+          <ButtonAction
+            :action="action"
+            :on-action="onAction"
+            icon="icon-qingkong2"
+            icon-size="14"
+            name="list.actions.all-clear"
+          />
+        </template>
+
+        <template #name="{ row, record }">
+          <!-- :to="`/filelist/all/${record.type}/${record.id}`" -->
+          <a-typography
+            class="netdisk-table-tr__name"
+            @click.stop="
+              onAction({
+                action: { key: 'show.info' },
+                record,
+              })
+            "
+          >
+            <IconFont
+              :type="
+                record.type === 'folder' ? 'icon-wenjianjia2' : 'icon-file2'
+              "
+              :size="18"
+              style="vertical-align: sub"
+            />
+            <span style="margin-left: 6px">{{ row }}</span>
+          </a-typography>
+        </template>
+
+        <!-- you can formatSize in formatList -->
+        <template #size="{ row, record }">
+          <span v-if="record.type === 'file'" class="netdisk-table-tr__size">
+            {{ formatSize(row) }}
+          </span>
+        </template>
+      </List>
+    </a-space>
+    <ModalForm ref="modalRef" @success="onSuccess" />
+  </div>
 </template>
+
+<style lang="less" scoped>
+  .container {
+    display: flex;
+    padding: 16px 20px;
+    padding-bottom: 0;
+    background-color: var(--color-fill-2);
+
+    ::v-deep {
+      .arco-table-td {
+        font-size: 13px;
+        border-bottom-color: rgb(226 226 226 / 30%);
+      }
+
+      .arco-auto-tooltip:has(.netdisk-table-tr__size) {
+        // float: right;
+        text-align: right;
+      }
+    }
+  }
+
+  .netdisk-table-tr__name {
+    color: rgb(var(--gray-10));
+    font-size: 13px;
+    text-decoration: none;
+    cursor: pointer;
+  }
+</style>
---
+++ src/views/filelist/shares/use-list.ts
@@ -0,0 +1,90 @@
+import { useI18n } from 'vue-i18n';
+import { IconPlus, IconDelete } from '@arco-design/web-vue/es/icon';
+import type { IColumn } from '@/components/list/types';
+import type { NodeRecord } from '@/api/filelist';
+
+export default function useList() {
+  const { t, d } = useI18n();
+
+  const columns: IColumn[] = [
+    {
+      title: 'filelist.name', // file.name
+      prop: 'name',
+      sortable: true,
+      // defaultSortOrder: 'descend' as const,
+      width: 200,
+    },
+    {
+      title: 'filelist.tags', // file.tag
+      prop: 'tags',
+      // sortable: true,
+      // defaultSortOrder: 'descend' as const,
+      // formatter: (date: string) => d(date, 'long'),
+      width: 100,
+    },
+    {
+      title: 'filelist.updated_at', // file.updated_at
+      prop: 'updated_at',
+      sortable: true,
+      // defaultSortOrder: 'descend' as const,
+      formatter: (date: string) => d(date, 'long'),
+      width: 200,
+    },
+    {
+      title: 'filelist.type', // file.type
+      prop: 'type',
+      formatter: (row, idx, record: NodeRecord) => {
+        // eslint error: camecase
+        const { file_extension: ext, type } = record;
+        if (ext) {
+          return `${ext} ${t('file')}`;
+        }
+        return type === 'folder' ? t('folder') : t('file');
+      },
+      width: 100,
+    },
+    {
+      title: 'filelist.size', // file.size
+      prop: 'size',
+      sortable: true,
+      formatter: (row, rowIndex, record: NodeRecord) => {
+        if (record.type === 'folder') {
+          return '';
+        }
+        return row;
+      },
+      width: 70,
+    },
+  ];
+
+  const toolbar = [
+    {
+      key: 'batch-delete',
+      icon: IconDelete,
+      type: 'text' as const,
+      bulk: true,
+      confirm: true,
+      confirmText: 'list.actions.confirm.batch-delete',
+    },
+    {
+      key: 'all-clear',
+      type: 'text' as const,
+      confirm: true,
+      confirmText: 'list.actions.confirm.all-clear',
+    },
+  ];
+
+  const actions = [
+    {
+      key: 'update',
+    },
+    {
+      key: 'delete',
+      status: 'danger' as const,
+      confirm: true,
+      confirmText: 'list.actions.confirm.delete',
+    },
+  ];
+
+  return { columns, toolbar, actions };
+}
