19ef1c219d56ae6663bf73a546b6f4d9dc9021e3
weimingfeng <luckywords@foxmail.com>
2023-03-12 18:45:59 +0800

    feat: add form locale

---
+++ src/components/form/locale/en-US.ts
@@ -0,0 +1,7 @@
+export default {
+  'form.actions.submit': 'Submit',
+  'form.actions.cancel': 'Cancel',
+  'form.actions.reset': 'Reset',
+  'form.actions.close': 'Close',
+  'form.actions.submit.success': 'Success',
+};
---
+++ src/components/form/locale/zh-CN.ts
@@ -0,0 +1,7 @@
+export default {
+  'form.actions.submit': '确定',
+  'form.actions.cancel': '取消',
+  'form.actions.reset': '重置',
+  'form.actions.close': '关闭',
+  'form.actions.submit.success': '操作成功',
+};
