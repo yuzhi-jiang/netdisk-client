9e8c359da46958764bedcc360f393ba836ef2e8c
weimingfeng <luckywords@foxmail.com>
2023-03-12 23:52:27 +0800

    feat: add shares locale

---
+++ src/views/filelist/shares/locale/en-US.ts
@@ -0,0 +1,5 @@
+export default {
+  'recycle.button.recover': 'Recover',
+  'recycle.button.delete': 'Delete',
+  'recycle.button.clear': 'Clear',
+};
---
+++ src/views/filelist/shares/locale/zh-CN.ts
@@ -0,0 +1,5 @@
+export default {
+  'recycle.button.recover': '恢复',
+  'recycle.button.delete': '删除',
+  'recycle.button.clear': '清空',
+};
