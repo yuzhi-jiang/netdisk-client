4adaab78f847698e910530ae40e0d580b99891d4
weimingfeng <luckywords@foxmail.com>
2023-03-17 15:04:20 +0800

    feat: update login page and form

--- src/views/login/components/login-form.vue
+++ src/views/login/components/login-form.vue
@@ -1,84 +1,169 @@
 <template>
   <div class="login-form-wrapper">
     <div class="login-form-title">{{ $t('login.form.title') }}</div>
-    <!-- <div class="login-form-sub-title">{{ $t('login.form.title') }}</div> -->
     <div class="login-form-error-msg">{{ errorMessage }}</div>
-    <a-form
-      ref="loginForm"
-      :model="userInfo"
-      class="login-form"
-      layout="vertical"
-      @submit="handleSubmit"
-    >
-      <a-form-item
-        field="username"
-        :rules="[{ required: true, message: $t('login.form.userName.errMsg') }]"
-        :validate-trigger="['change', 'blur']"
-        hide-label
-      >
-        <a-input
-          v-model="userInfo.username"
-          :placeholder="$t('login.form.userName.placeholder')"
+
+    <a-tabs v-model:active-key="state.loginType" type="line" size="medium">
+      <a-tab-pane key="account" title="账号登录">
+        <a-form
+          ref="loginForm"
+          :model="userInfo"
+          class="login-form"
+          layout="vertical"
+          @submit="onSubmit"
         >
-          <template #prefix>
-            <icon-user />
-          </template>
-        </a-input>
-      </a-form-item>
-      <a-form-item
-        field="password"
-        :rules="[{ required: true, message: $t('login.form.password.errMsg') }]"
-        :validate-trigger="['change', 'blur']"
-        hide-label
-      >
-        <a-input-password
-          v-model="userInfo.password"
-          :placeholder="$t('login.form.password.placeholder')"
-          allow-clear
-        >
-          <template #prefix>
-            <icon-lock />
-          </template>
-        </a-input-password>
-      </a-form-item>
-      <a-space :size="16" direction="vertical">
-        <div class="login-form-password-actions">
-          <a-checkbox
-            checked="rememberPassword"
-            :model-value="loginConfig.rememberPassword"
-            @change="setRememberPassword as any"
+          <a-form-item
+            hide-label
+            field="username"
+            :rules="[{ required: true, message: '请输入用户名' }]"
+            :validate-trigger="['change', 'blur']"
           >
-            {{ $t('login.form.rememberPassword') }}
-          </a-checkbox>
-          <a-link>{{ $t('login.form.forgetPassword') }}</a-link>
-        </div>
-        <a-button type="primary" html-type="submit" long :loading="loading">
-          {{ $t('login.form.login') }}
-        </a-button>
-      </a-space>
-      <a-form-item>
-        <a-divider orientation="center">其他方式</a-divider>
-      </a-form-item>
-      <a-form-item>
-        <a-space :size="24" fill align="center">
-          <a-tooltip content="微信" mini>
-            <icon-wechat size="18" style="cursor: pointer" />
-          </a-tooltip>
-          <a-tooltip content="QQ" mini>
-            <icon-qq size="18" />
-          </a-tooltip>
-          <a-tooltip content="Github" mini>
-            <icon-github size="18" />
-          </a-tooltip>
-          <a-tooltip content="微博" mini>
-            <icon-weibo size="18" />
-          </a-tooltip>
-          <a-button type="text" class="login-form-register-btn">
-            没有账号？点击{{ $t('login.form.register') }}
-          </a-button>
-        </a-space>
-      </a-form-item>
-    </a-form>
+            <a-input v-model="userInfo.username" placeholder="请输入用户名">
+              <template #prefix>
+                <icon-user />
+              </template>
+            </a-input>
+          </a-form-item>
+          <a-form-item
+            field="password"
+            :rules="[{ required: true, message: '请输入密码' }]"
+            :validate-trigger="['change', 'blur']"
+            hide-label
+          >
+            <a-input-password
+              v-model="userInfo.password"
+              placeholder="请输入密码"
+              allow-clear
+            >
+              <template #prefix>
+                <icon-lock />
+              </template>
+            </a-input-password>
+          </a-form-item>
+          <a-space :size="16" direction="vertical">
+            <div class="login-form-password-actions">
+              <a-checkbox
+                checked="rememberPassword"
+                :model-value="loginConfig.rememberPassword"
+                @change="setRememberPassword as any"
+              >
+                {{ $t('login.form.rememberPassword') }}
+              </a-checkbox>
+              <a-link>{{ $t('login.form.forgetPassword') }}</a-link>
+            </div>
+            <a-button type="primary" html-type="submit" long :loading="loading">
+              {{ $t('login.form.login') }}
+            </a-button>
+          </a-space>
+        </a-form>
+      </a-tab-pane>
+      <a-tab-pane key="mobile" title="短信登录">
+        <a-form
+          ref="loginForm"
+          :model="userInfo"
+          class="login-form"
+          layout="vertical"
+          @submit="onSubmit"
+        >
+          <a-form-item
+            hide-label
+            field="mobile"
+            :rules="[
+              { required: true, message: '请输入手机号' },
+              {
+                validator: (v = '', cb) => {
+                  if (isMobile(v.trim())) {
+                    cb();
+                    return;
+                  }
+                  cb('请输入正确的手机号码');
+                },
+              },
+            ]"
+            :validate-trigger="['change', 'blur']"
+          >
+            <a-input v-model="userInfo.mobile" placeholder="请输入手机号">
+              <template #prefix>
+                <icon-mobile />
+              </template>
+              <template #suffix>
+                <a-tooltip mini content="默认注册账号">
+                  <icon-info-circle style="cursor: pointer" />
+                </a-tooltip>
+              </template>
+            </a-input>
+          </a-form-item>
+          <a-form-item
+            field="captcha"
+            :rules="[{ required: true, message: '请输入验证码' }]"
+            :validate-trigger="['change', 'blur']"
+            hide-label
+          >
+            <a-input
+              v-model="userInfo.captcha"
+              placeholder="请输入验证码"
+              allow-clear
+            >
+              <template #prefix>
+                <icon-lock />
+              </template>
+              <template #append>
+                <a-button
+                  :disabled="state.captchaTime !== 0"
+                  size="mini"
+                  long
+                  @click="onCaptchaClick"
+                >
+                  {{ state.captchaTime !== 0 ? `${state.captchaTime}s` : '' }}
+                  获取验证码
+                </a-button>
+              </template>
+            </a-input>
+          </a-form-item>
+          <a-space :size="16" direction="vertical">
+            <a-button type="primary" html-type="submit" long :loading="loading">
+              {{ $t('login.form.login') }}
+            </a-button>
+          </a-space>
+        </a-form>
+      </a-tab-pane>
+    </a-tabs>
+
+    <a-divider orientation="center" :margin="32">其他方式</a-divider>
+
+    <a-space :size="24" fill align="center">
+      <a-tooltip content="微信" mini>
+        <icon-wechat
+          size="18"
+          style="cursor: pointer"
+          @click="customerLogin('wx')"
+        />
+      </a-tooltip>
+      <a-tooltip content="QQ" mini>
+        <icon-qq
+          size="18"
+          style="cursor: pointer"
+          @click="customerLogin('qq')"
+        />
+      </a-tooltip>
+      <a-tooltip content="Github" mini>
+        <icon-github
+          size="18"
+          style="cursor: pointer"
+          @click="customerLogin('github')"
+        />
+      </a-tooltip>
+      <a-tooltip content="微博" mini>
+        <icon-weibo
+          size="18"
+          style="cursor: pointer"
+          @click="customerLogin('weibo')"
+        />
+      </a-tooltip>
+      <a-button type="text" class="login-form-register-btn" @click="register">
+        没有账号？点击{{ $t('login.form.register') }}
+      </a-button>
+    </a-space>
   </div>
 </template>
 
@@ -92,6 +177,8 @@
   import { useUserStore } from '@/store';
   import useLoading from '@/hooks/loading';
   import type { LoginData } from '@/api/user';
+  import { isMobile } from '@/utils/validate';
+  import { getCaptcha, LoginType } from '@/api/user';
 
   const router = useRouter();
   const { t } = useI18n();
@@ -101,49 +188,118 @@
 
   const loginConfig = useStorage('login-config', {
     rememberPassword: true,
-    username: 'admin', // 演示默认值
-    password: 'admin', // demo default value
+    username: '', // 演示默认值
+    password: '', // demo default value
   });
   const userInfo = reactive({
     username: loginConfig.value.username,
     password: loginConfig.value.password,
+    mobile: '',
+    captcha: '' as any,
+  });
+  const state = reactive<{
+    captchaTime: number;
+    loginType: 'account' | 'mobile';
+  }>({
+    captchaTime: 0,
+    loginType: 'account',
   });
 
-  const handleSubmit = async ({
+  const setRememberPassword = (value: boolean) => {
+    loginConfig.value.rememberPassword = value;
+  };
+
+  const handleSubmit = async (values: Record<string, any>) => {
+    setLoading(true);
+
+    const remember = () => {
+      const { rememberPassword } = loginConfig.value;
+      const { username, password } = values;
+      // 实际生产环境需要进行加密存储。
+      // The actual production environment requires encrypted storage.
+      loginConfig.value.username = rememberPassword ? username : '';
+      loginConfig.value.password = rememberPassword ? password : '';
+    };
+
+    const { loginType } = state;
+
+    if (loginType === 'mobile') {
+      delete values.username;
+      delete values.password;
+      values.type = LoginType.MOBILE_CAPTCHA;
+    } else if (loginType === 'account') {
+      delete values.mobile;
+      delete values.captcha;
+      values.type = LoginType.ACCOUNT;
+    }
+
+    try {
+      await userStore.login(values as LoginData);
+      const { redirect, ...othersQuery } = router.currentRoute.value.query;
+      router.push({
+        name: (redirect as string) || 'filelist',
+        query: {
+          ...othersQuery,
+        },
+      });
+      Message.success(t('login.form.login.success'));
+
+      if (loginType === 'account') remember();
+    } catch (err) {
+      errorMessage.value = (err as Error).message;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const onSubmit = ({
     errors,
     values,
   }: {
     errors: Record<string, ValidatedError> | undefined;
     values: Record<string, any>;
   }) => {
-    if (loading.value) return;
-    if (!errors) {
-      setLoading(true);
-      try {
-        await userStore.login(values as LoginData);
-        const { redirect, ...othersQuery } = router.currentRoute.value.query;
-        router.push({
-          name: (redirect as string) || 'Workplace',
-          query: {
-            ...othersQuery,
-          },
-        });
-        Message.success(t('login.form.login.success'));
-        const { rememberPassword } = loginConfig.value;
-        const { username, password } = values;
-        // 实际生产环境需要进行加密存储。
-        // The actual production environment requires encrypted storage.
-        loginConfig.value.username = rememberPassword ? username : '';
-        loginConfig.value.password = rememberPassword ? password : '';
-      } catch (err) {
-        errorMessage.value = (err as Error).message;
-      } finally {
-        setLoading(false);
-      }
-    }
+    if (loading.value || errors) return;
+    console.log(values);
+    handleSubmit(values);
   };
-  const setRememberPassword = (value: boolean) => {
-    loginConfig.value.rememberPassword = value;
+
+  const onCaptchaClick = async () => {
+    if (!isMobile(userInfo.mobile)) {
+      Message.error('请输入正确的手机号');
+      return;
+    }
+
+    await getCaptcha(userInfo.mobile);
+    state.captchaTime = 60;
+    const timer = setInterval(() => {
+      state.captchaTime -= 1;
+      if (state.captchaTime === 0) {
+        clearInterval(timer);
+      }
+    }, 1000);
+  };
+
+  const customerLogin = async (type: 'wx' | 'qq' | 'github' | 'weibo') => {
+    window.open();
+    const handleOpenWindow = (e: MessageEvent) => {
+      const { data } = e;
+      console.log(data);
+      window.removeEventListener('message', handleOpenWindow);
+      // 跳转路由
+    };
+
+    window.addEventListener('message', handleOpenWindow);
+  };
+
+  const register = () => {
+    router.push({
+      name: 'register',
+      // 若用户已经输入账号密码，则携带进入注册页面
+      state: {
+        userInfo,
+      },
+    });
   };
 </script>
 
