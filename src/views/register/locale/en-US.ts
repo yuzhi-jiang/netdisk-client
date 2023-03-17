6229b8712c9560ac0ffc39f87b55f88b2f6bed51
weimingfeng <luckywords@foxmail.com>
2023-03-17 15:07:59 +0800

    feat: add register page and component

---
+++ src/views/register/locale/en-US.ts
@@ -0,0 +1,13 @@
+export default {
+  'login.form.title': 'Login to Netdisk',
+  'login.form.userName.errMsg': 'Username cannot be empty',
+  'login.form.password.errMsg': 'Password cannot be empty',
+  'login.form.login.errMsg': 'Login error, refresh and try again',
+  'login.form.login.success': 'welcome to use',
+  'login.form.userName.placeholder': 'Please enter the username',
+  'login.form.password.placeholder': 'Please enter the password',
+  'login.form.rememberPassword': 'Remember password',
+  'login.form.forgetPassword': 'Forgot password',
+  'login.form.login': 'login',
+  'login.form.register': 'register account',
+};
