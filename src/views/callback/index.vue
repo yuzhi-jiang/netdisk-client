d9b2974e20f648e0a4dd4d73156052bd5c3336f0
weimingfeng <luckywords@foxmail.com>
2023-03-17 15:04:46 +0800

    feat: add callback page

---
+++ src/views/callback/index.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+  import { useRouter } from 'vue-router';
+  import qs from 'query-string';
+  import axios from 'axios';
+  import { onMounted } from 'vue';
+
+  const router = useRouter();
+  const route = router.currentRoute;
+  console.log(window.location.href);
+
+  onMounted(async () => {
+    const {
+      query: { code },
+    } = qs.parseUrl(window.location.href);
+    const res = await axios.post(
+      'http://192.168.196.80:8082/front/oauth/github/callback',
+      {
+        code,
+      }
+    );
+    console.log(res);
+  });
+</script>
+
+<template>
+  {{ route }}
+</template>
