b3c509ba8961890d7ed053754824f9a8a377da11
weimingfeng <luckywords@foxmail.com>
2023-03-18 21:02:23 +0800

    fix: remove locale

--- src/locale/zh-CN.ts
+++ src/locale/zh-CN.ts
@@ -18,7 +18,7 @@ import locale403 from '@/views/exception/403/locale/zh-CN';
 import locale404 from '@/views/exception/404/locale/zh-CN';
 import locale500 from '@/views/exception/500/locale/zh-CN';
 
-import localeUserInfo from '@/views/user/info/locale/zh-CN';
+// import localeUserInfo from '@/views/user/info/locale/zh-CN';
 import localeUserSetting from '@/views/user/setting/locale/zh-CN';
 
 // customer
@@ -92,7 +92,6 @@ export default {
   ...locale403,
   ...locale404,
   ...locale500,
-  ...localeUserInfo,
   ...localeUserSetting,
 
   // customer
