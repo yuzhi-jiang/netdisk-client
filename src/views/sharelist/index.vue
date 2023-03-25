c19e00b688a7936c5dd6269852d64301840a3f19
weimingfeng <luckywords@foxmail.com>
2023-03-25 13:43:27 +0800

    feat: add share info logic

--- src/views/sharelist/index.vue
+++ src/views/sharelist/index.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
   import { ref } from 'vue';
-  import { useRoute } from 'vue-router';
+  import { useRoute, useRouter } from 'vue-router';
   import { NodeRecord } from '@/api/filelist';
   import {
     IShareRouteReqParams,
+    getShareToken,
+    getShareInfo,
     getOtherShares,
     putOtherShares,
   } from '@/api/shares';
@@ -11,18 +13,29 @@
   import { IAction } from '@/components/list/types';
   import MoveForm from '@/views/components/move-form.vue';
   import { useUserStore } from '@/store';
+  import useVisible from '@/hooks/visible';
+  import useLoading from '@/hooks/loading';
   import { formatList, formatSize, paramsAdapter } from './utils';
   import useList from './use-list';
   import ButtonAction from './components/button-action.vue';
 
   const $route = useRoute();
+  const router = useRouter();
   const userStore = useUserStore();
   const { columns, toolbar, actions } = useList();
+  const { visible, setVisible } = useVisible();
+  const { loading, setLoading } = useLoading();
   const listRef = ref<InstanceType<typeof List>>();
   const moveRef = ref<InstanceType<typeof MoveForm>>();
+  const form = ref({
+    sharePwd: '' as string,
+  });
   const states = {
     reqParams: {} as IShareRouteReqParams, // request params
     moveableFileDiskId: '' as string,
+    shareToken: '' as string,
+    hasPwd: true,
+    sharePwd: '' as string,
   };
 
   const onAction = async ({
@@ -46,6 +59,13 @@
           moveableFileDiskId: diskId,
         } as any);
         break;
+
+      case 'watch-netdisk':
+        // redirectHome();
+        router.push({
+          name: 'filelist',
+        });
+        break;
       default:
         break;
     }
@@ -55,10 +75,72 @@
     listRef.value?.reload();
   };
 
+  /**
+   * 获取token
+   * 从 form 中取出输入的密码
+   * 从 states 中 获取请求参数 shareId
+   */
+  const handleToken = async () => {
+    const {
+      reqParams: { shareId },
+    } = states;
+    const { sharePwd } = form.value;
+    states.sharePwd = sharePwd;
+
+    try {
+      setLoading(true);
+      const {
+        data: { shareToken },
+      } = await getShareToken({ shareId, sharePwd });
+      states.shareToken = shareToken;
+      setVisible(false);
+      listRef.value?.reload();
+    } catch (e) {
+      setVisible(true);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  /**
+   * 获取请求信息，是否需要密码
+   * 需要密码则弹窗输入密码
+   * 如不需要密码直接请求token
+   */
+  const beforeRequest = async () => {
+    const {
+      reqParams: { shareId },
+    } = states;
+    const {
+      data: { hasPwd },
+    } = await getShareInfo(shareId);
+    states.hasPwd = hasPwd;
+    // const hasPwd = true;
+
+    if (hasPwd) {
+      setVisible(true);
+      return;
+    }
+    await handleToken();
+  };
+
   const request = async (params = {}) => {
     params = paramsAdapter(params as any, states);
     // console.log(params);
-    const { data } = await getOtherShares(params as any);
+
+    /**
+     * 处理密码情况
+     */
+    const { hasPwd, sharePwd } = states;
+    if (hasPwd && !sharePwd) {
+      setVisible(true);
+      return {
+        data: [],
+        total: 0,
+      };
+    }
+
+    const { data } = await getOtherShares(params as any, states.shareToken);
     if (data?.list) {
       const res = formatList(data.list);
       states.moveableFileDiskId = res[0].diskId as string;
@@ -102,6 +184,7 @@
         :columns="columns"
         :actions="[]"
         :request="request"
+        :before-request="beforeRequest"
         row-key="fileId"
         @action="onAction"
       >
@@ -154,6 +237,30 @@
       :request="putOtherShares"
       @success="onSuccess"
     ></MoveForm>
+
+    <a-modal
+      :visible="visible"
+      :closable="false"
+      :ok-loading="loading"
+      title-align="start"
+      hide-cancel
+      @ok="handleToken"
+    >
+      <template #title> 输入密码 </template>
+      <a-form :model="form">
+        <a-form-item
+          label="密码"
+          field="sharePwd"
+          :rules="{
+            required: true,
+            message: '请输入密码',
+          }"
+          validate-trigger="input"
+        >
+          <a-input v-model="form.sharePwd" placeholder="请输入密码" />
+        </a-form-item>
+      </a-form>
+    </a-modal>
   </div>
 </template>
 
