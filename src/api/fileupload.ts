cbe1b9fcccf29ac567595cb01a3967309e0a5ec7
weimingfeng <luckywords@foxmail.com>
2023-04-06 23:06:34 +0800

    feat: add upload method

--- src/api/fileupload.ts
+++ src/api/fileupload.ts
@@ -1,25 +1,27 @@
 import axios, { AxiosResponse } from 'axios';
 
+const VITE_API_BASE_URL = process.env['VITE_API_BASE_URL'];
+
 export type FileParams = {
-    checkNameMode: 'auto_rename' | 'overwrite' | 'refuse';
-    parentFileId: string;
-    diskId: string;
-    fileName: string;
-    type: 'folder' | 'file';
-    hash: string;
+  checkNameMode: 'auto_rename' | 'overwrite' | 'refuse';
+  parentFileId: string;
+  diskId: string;
+  fileName: string;
+  type: 'folder' | 'file';
+  hash: string;
 };
 
 export type uploadPartParams = Partial<{
-    file: File,
-    token: string,
-    uploadId: string
-}>
+  file: File;
+  token: string;
+  uploadId: string;
+}>;
 
 export type completeParams = {
-    fileId: string,
-    diskId: string,
-    uploadId: string
-}
+  fileId: string;
+  diskId: string;
+  uploadId: string;
+};
 /**
  * 
  * 
@@ -34,11 +36,14 @@ export type completeParams = {
  */
 
 export function createWithFolders(params: FileParams) {
-    return axios.post('/front/file/createWithFolders', params);
+  return axios.post('/front/file/createWithFolders', params);
 }
+
+export const XHR_UPLOAD_URL = `${VITE_API_BASE_URL}/front/file/uploadPart`;
+
 export function uploadPart(params: uploadPartParams) {
-    return axios.post('/front/file/uploadPart', params);
+  return axios.post('/front/file/uploadPart', params);
 }
 export function complete(params: completeParams) {
-    return axios.post('/front/file/complete', params);
+  return axios.post('/front/file/complete', params);
 }
--- src/views/filelist/all/components/upload-form.vue
+++ src/views/filelist/all/components/upload-form.vue
@@ -1,136 +1,174 @@
 <script lang="ts" setup>
-import { useRoute } from 'vue-router';
-import useLoading from '@/hooks/loading';
-import useVisible from '@/hooks/visible';
-import { Message, RequestOption, UploadRequest } from '@arco-design/web-vue';
-import { has } from 'lodash';
-import { createWithFolders, uploadPart, complete } from '@/api/fileupload';
-import { FileParams, uploadPartParams } from '@/api/fileupload';
-import { getfilehash } from '../../../../utils/file'
+  import { useRoute } from 'vue-router';
+  import useLoading from '@/hooks/loading';
+  import useVisible from '@/hooks/visible';
+  import { Message, RequestOption, UploadRequest } from '@arco-design/web-vue';
+  import { has } from 'lodash';
+  import {
+    createWithFolders,
+    uploadPart,
+    complete,
+    XHR_UPLOAD_URL,
+  } from '@/api/fileupload';
+  import { FileParams, uploadPartParams } from '@/api/fileupload';
+  import { getfilehash } from '@/utils/file';
 
-defineEmits(['success']);
-const defFromdata: FileParams = {
-  checkNameMode: 'auto_rename',
-  parentFileId: '',
-  diskId: '',
-  fileName: '',
-  type: 'file',
-  hash: ''
-};
-const defUploadPart: uploadPartParams = {
-  file: undefined,
-  token: undefined,
-  uploadId: undefined
-}
-const route = useRoute();
-const { visible, setVisible } = useVisible();
-const createWithFolder = (data: FileParams) => {
-  return createWithFolders(data)
-}
-const xhrRequest = async (xhr: XMLHttpRequest, formData: FormData) => {
-
-
-
-  // 计算文件 SHA1 值
-  const file = formData.get('file') as File;
-  const hash = await getfilehash(file)
-  console.log(hash)
-  defFromdata.hash = hash
-  defFromdata.fileName=file.name
-  const res: any = await createWithFolder(defFromdata)
-  if (res.rapidUpload == true) {
-    //快速上传
-    try {
-      xhr.abort();
-      
-    } catch (error) {
-      Message.error('上传错误');
-    }
-    return
-  }
-
-
-
-
-  xhr.open('post', '//upload-z2.qbox.me/', true);
-
-  /**
-   * 第二步 上传
-   */
-  // await getids() {}
-  // xhr.send(formData);
-
-  try {
-    xhr.abort();
-  } catch (error) {
-    Message.error('上传错误');
-  }
-
-  /**
-   * 第三步 确认
-   */
-  Message.success('上传成功');
-};
-
-const customRequest = (option: RequestOption): UploadRequest => {
-  const { onProgress, onError, onSuccess, fileItem, name } = option;
-
-  const xhr = new XMLHttpRequest();
-  if (xhr.upload) {
-    xhr.upload.onprogress = function (event) {
-      let percent = 0;
-      if (event.total > 0) {
-        /**
-         * 百分比
-         */
-        percent = event.loaded / event.total;
-      }
-      onProgress(percent, event);
-    };
-  }
-  xhr.onerror = function error(e) {
-    onError(e);
+  defineEmits(['success']);
+  const route = useRoute();
+  const { visible, setVisible } = useVisible();
+  const defFromdata: FileParams = {
+    checkNameMode: 'auto_rename',
+    parentFileId: '',
+    diskId: '',
+    fileName: '',
+    type: 'file',
+    hash: '',
   };
-  xhr.onload = function onload() {
-    if (xhr.status < 200 || xhr.status >= 300) {
-      onError(xhr.responseText);
+  const defUploadPart: uploadPartParams = {
+    file: undefined,
+    token: undefined,
+    uploadId: undefined,
+  };
+
+  const xhrRequest = async (xhr: XMLHttpRequest, option: RequestOption) => {
+    const { onError, onSuccess, onProgress, fileItem, name } = option;
+
+    if (xhr.upload) {
+      xhr.upload.onprogress = (event) => {
+        let percent = 0;
+        if (event.total > 0) {
+          /**
+           * 百分比
+           */
+          percent = event.loaded / event.total;
+        }
+        onProgress(percent, event);
+      };
+    }
+
+    xhr.onerror = (e) => {
+      onError(e);
+    };
+
+    xhr.onload = async () => {
+      if (xhr.status < 200 || xhr.status >= 300) {
+        onError(xhr.responseText);
+        return;
+      }
+      /**
+       * 第三步 确认
+       */
+      Message.success('上传成功');
+
+      const {
+        data: { fileId },
+      } = JSON.parse(xhr.response);
+
+      try {
+        const { code, msg }: any = await complete({
+          fileId,
+          uploadId,
+          diskId: defFromdata.diskId,
+        });
+        if (code === 501) {
+          Message.error(msg);
+          return;
+        }
+        onSuccess(xhr.response);
+      } catch (error) {
+        Message.error('第三步错误');
+      }
+    };
+
+    /**
+     * 计算文件 SHA1 值
+     */
+    const hash = await getfilehash(fileItem.file);
+    console.log(hash);
+
+    defFromdata.hash = hash;
+    defFromdata.fileName = name as string;
+
+    /**
+     * 第一步，判断是否可以快速上传
+     */
+
+    let step1Res: any;
+    try {
+      const res = await createWithFolders(defFromdata);
+      step1Res = res.data;
+    } catch (error) {
+      Message.error('第一步出错');
       return;
     }
-    onSuccess(xhr.response);
+
+    if (step1Res.rapidUpload == true) {
+      // 快速上传
+      try {
+        xhr.abort();
+        Message.success('上传成功');
+      } catch (error) {
+        Message.error('上传错误');
+      }
+      return;
+    }
+
+    /**
+     * 第二步：上传文件
+     */
+    const { token, uploadId } = step1Res;
+    let fileId: string;
+    try {
+      const formData = new FormData();
+      formData.append('file', fileItem.file as File);
+      formData.append('token', token);
+      formData.append('uploadId', uploadId);
+      xhr.open('post', XHR_UPLOAD_URL, true);
+      xhr.send(formData);
+    } catch (error) {
+      Message.error('第二步骤出错');
+      return;
+    }
   };
-  const formData = new FormData();
-  if (fileItem.file) {
-    formData.append('file', fileItem.file);
-  }
-  // formData.append(name || 'file', fileItem.file);
-  // if (typeof name === 'string' && fileItem.file) {
 
-  // } else if (typeof name === 'function' && fileItem.file) {
-  //   formData.append(name(fileItem), fileItem.file);
-  // }
+  const customRequest = (option: RequestOption): UploadRequest => {
+    const xhr = new XMLHttpRequest();
 
-  xhrRequest(xhr, formData);
+    // formData.append(name || 'file', fileItem.file);
+    // if (typeof name === 'string' && fileItem.file) {
 
-  const abort = () => {
-    xhr.abort();
+    // } else if (typeof name === 'function' && fileItem.file) {
+    //   formData.append(name(fileItem), fileItem.file);
+    // }
+
+    xhrRequest(xhr, option);
+
+    const abort = () => {
+      xhr.abort();
+    };
+
+    return {
+      abort,
+    };
   };
 
-  return {
-    abort,
+  const init = async (diskId: string, parentFileId: string) => {
+    defFromdata.diskId = diskId;
+    defFromdata.parentFileId = parentFileId;
+    setVisible(true);
   };
-};
 
-const init = async (diskId: string, parentFileId: string) => {
-  defFromdata.diskId = diskId;
-  defFromdata.parentFileId = parentFileId;
-  setVisible(true);
-};
-
-defineExpose({ init });
+  defineExpose({ init });
 </script>
 
 <template>
-  <a-modal v-model:visible="visible" title="上传" :footer="false" :unmount-on-close="true" title-align="start">
+  <a-modal
+    v-model:visible="visible"
+    title="上传"
+    :footer="false"
+    :unmount-on-close="true"
+    title-align="start"
+  >
     <a-upload :custom-request="customRequest" draggable></a-upload>
   </a-modal>
 </template>
